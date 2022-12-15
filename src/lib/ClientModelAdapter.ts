b719a822b30ba80930c149e3b8fbca497ab57902
pierrecabriere <hello@pierrecabriere.fr>
2022-12-15 17:45:56 +0100

    add serializer

--- src/lib/ClientModelAdapter.ts
+++ src/lib/ClientModelAdapter.ts
@@ -1,12 +1,169 @@
-import { DataModel, Model, ModelAdapter, ModelList } from "@graphand/core";
+import {
+  DataModel,
+  Model,
+  ModelAdapter,
+  ModelAdapterFetcher,
+  ModelList,
+} from "@graphand/core";
 import Client from "./Client";
-import { BehaviorSubject } from "rxjs";
 
 class ClientModelAdapter<
   T extends typeof Model = typeof Model
 > extends ModelAdapter<T> {
   static __client: Client;
   private __instancesMap: Map<string, InstanceType<T>>;
+
+  fetcher: ModelAdapterFetcher<T> = {
+    count: async (query) => {
+      if (!this.client) {
+        throw new Error("MODEL_NO_CLIENT");
+      }
+
+      return await this.client.fetch(`${this.model.slug}/count`, {
+        method: "POST",
+        body: JSON.stringify(query),
+      });
+    },
+    get: async (query = {}) => {
+      if (!this.client) {
+        throw new Error("MODEL_NO_CLIENT");
+      }
+
+      if (typeof query === "string") {
+        const res = await this.client.fetch(`${this.model.slug}/${query}`, {
+          method: "GET",
+        });
+
+        return this.mapOrNew(res);
+      } else {
+        query.pageSize = 1;
+
+        const list = await this.fetcher.getList(query);
+
+        if (!list) {
+          throw new Error();
+        }
+
+        return list[0] as InstanceType<T>;
+      }
+    },
+    getList: async (query) => {
+      if (!this.client) {
+        throw new Error("MODEL_NO_CLIENT");
+      }
+
+      const res = await this.client.fetch(`${this.model.slug}/query`, {
+        method: "POST",
+        body: JSON.stringify(query),
+      });
+
+      const documents = res.rows.map((r) => this.mapOrNew(r));
+      const count = res.count;
+
+      return new ModelList<InstanceType<T>>(this.model, documents, count);
+    },
+    createOne: async (payload) => {
+      if (!this.client) {
+        throw new Error("MODEL_NO_CLIENT");
+      }
+
+      const res = await this.client.fetch(this.model.slug, {
+        method: "POST",
+        body: JSON.stringify(payload),
+      });
+
+      return this.mapOrNew(res);
+    },
+    createMultiple: async (payload) => {
+      if (!this.client) {
+        throw new Error("MODEL_NO_CLIENT");
+      }
+
+      const res = await this.client.fetch(this.model.slug, {
+        method: "POST",
+        body: JSON.stringify(payload),
+      });
+
+      return res.map((r) => this.mapOrNew(r));
+    },
+    updateOne: async (query, update) => {
+      if (!this.client) {
+        throw new Error("MODEL_NO_CLIENT");
+      }
+
+      if (typeof query === "string") {
+        const res = await this.client.fetch(`${this.model.slug}/${query}`, {
+          method: "PATCH",
+          body: JSON.stringify({ update }),
+        });
+
+        return this.mapOrNew(res);
+      } else {
+        query.pageSize = 1;
+
+        const list = await this.fetcher.updateMultiple(query, update);
+
+        if (!list) {
+          throw new Error();
+        }
+
+        return list[0] as InstanceType<T>;
+      }
+    },
+    updateMultiple: async (query, update) => {
+      if (!this.client) {
+        throw new Error("MODEL_NO_CLIENT");
+      }
+
+      const res = await this.client.fetch(this.model.slug, {
+        method: "PATCH",
+        body: JSON.stringify({ ...query, update }),
+      });
+
+      return res.map((r) => this.mapOrNew(r) as InstanceType<T>);
+    },
+    deleteOne: async (query) => {
+      if (!this.client) {
+        throw new Error("MODEL_NO_CLIENT");
+      }
+
+      if (typeof query === "string") {
+        const res = await this.client.fetch(`${this.model.slug}/${query}`, {
+          method: "DELETE",
+        });
+
+        const deleted = res === 1;
+        if (deleted) {
+          this.instancesMap.delete(query);
+        }
+
+        return deleted;
+      } else {
+        query.pageSize = 1;
+
+        const res = await this.fetcher.deleteMultiple(query);
+
+        res.forEach(this.instancesMap.delete);
+
+        return res?.length === 1;
+      }
+    },
+    deleteMultiple: async (query) => {
+      if (!this.client) {
+        throw new Error("MODEL_NO_CLIENT");
+      }
+
+      const res = await this.client.fetch(this.model.slug, {
+        method: "DELETE",
+        body: JSON.stringify(query),
+      });
+
+      res.forEach((_id) => this.instancesMap.delete(_id));
+
+      return res;
+    },
+    loadSchema: async () => {},
+  };
 
   constructor() {
     super();
@@ -31,7 +188,7 @@ class ClientModelAdapter<
     let i;
 
     if (payload._id) {
-      i = this.instancesMap.get(String(payload._id));
+      i = this.instancesMap.get(payload._id);
     }
 
     if (i) {
@@ -43,158 +200,10 @@ class ClientModelAdapter<
       }
     } else {
       i = new this.model(payload);
-      this.instancesMap.set(String(payload._id), i);
+      this.instancesMap.set(payload._id, i);
     }
 
     return i;
-  }
-
-  async count(query) {
-    if (!this.client) {
-      throw new Error("MODEL_NO_CLIENT");
-    }
-
-    return await this.client.fetch(`${this.model.slug}/count`, {
-      method: "POST",
-      body: JSON.stringify(query),
-    });
-  }
-
-  async get(query) {
-    if (!this.client) {
-      throw new Error("MODEL_NO_CLIENT");
-    }
-
-    if (typeof query === "string") {
-      const res = await this.client.fetch(`${this.model.slug}/${query}`, {
-        method: "GET",
-      });
-
-      return this.mapOrNew(res);
-    } else {
-      query.pageSize = 1;
-
-      const list = await this.getList(query);
-
-      if (!list) {
-        return null;
-      }
-
-      return list[0] as InstanceType<T>;
-    }
-  }
-
-  async getList(query) {
-    if (!this.client) {
-      throw new Error("MODEL_NO_CLIENT");
-    }
-
-    const res = await this.client.fetch(`${this.model.slug}/query`, {
-      method: "POST",
-      body: JSON.stringify(query),
-    });
-
-    const documents = res.rows.map((r) => this.mapOrNew(r));
-    const count = res.count;
-
-    return new ModelList<InstanceType<T>>(this.model, documents, count);
-  }
-
-  async createOne(payload) {
-    if (!this.client) {
-      throw new Error("MODEL_NO_CLIENT");
-    }
-
-    const res = await this.client.fetch(this.model.slug, {
-      method: "POST",
-      body: JSON.stringify(payload),
-    });
-
-    return this.mapOrNew(res);
-  }
-
-  async createMultiple(payload) {
-    if (!this.client) {
-      throw new Error("MODEL_NO_CLIENT");
-    }
-
-    const res = await this.client.fetch(this.model.slug, {
-      method: "POST",
-      body: JSON.stringify(payload),
-    });
-
-    return res.map((r) => this.mapOrNew(r));
-  }
-
-  async updateOne(query, update) {
-    if (!this.client) {
-      throw new Error("MODEL_NO_CLIENT");
-    }
-
-    if (typeof query === "string") {
-      const res = await this.client.fetch(`${this.model.slug}/${query}`, {
-        method: "PATCH",
-        body: JSON.stringify({ update }),
-      });
-
-      return this.mapOrNew(res);
-    } else {
-      query.pageSize = 1;
-
-      const list = await this.updateMultiple(query, update);
-
-      if (!list) {
-        return null;
-      }
-
-      return list[0] as InstanceType<T>;
-    }
-  }
-
-  async updateMultiple(query, update) {
-    if (!this.client) {
-      throw new Error("MODEL_NO_CLIENT");
-    }
-
-    const res = await this.client.fetch(this.model.slug, {
-      method: "PATCH",
-      body: JSON.stringify({ ...query, update }),
-    });
-
-    return res.map((r) => this.mapOrNew(r) as InstanceType<T>);
-  }
-
-  async deleteOne(query): Promise<boolean> {
-    if (!this.client) {
-      throw new Error("MODEL_NO_CLIENT");
-    }
-
-    if (typeof query === "string") {
-      const res = await this.client.fetch(`${this.model.slug}/${query}`, {
-        method: "DELETE",
-      });
-
-      return res === 1;
-    } else {
-      query.pageSize = 1;
-
-      const res = await this.deleteMultiple(query);
-
-      return res === 1;
-    }
-  }
-
-  async deleteMultiple(query): Promise<number> {
-    if (!this.client) {
-      throw new Error("MODEL_NO_CLIENT");
-    }
-
-    const res = await this.client.fetch(this.model.slug, {
-      method: "DELETE",
-      body: JSON.stringify(query),
-    });
-
-    return res;
   }
 
   async loadSchema() {
