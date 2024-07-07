7936a5a993223edf9bf9ebef75f855eb1e1b4a8a
pierrecabriere <hello@pierrecabriere.fr>
2024-07-07 17:51:06 +0200

    add github ci for tests

--- packages/module-realtime/src/ModuleRealtime.spec.ts
+++ packages/module-realtime/src/ModuleRealtime.spec.ts
@@ -2,18 +2,13 @@ import { DataModel } from "@graphand/core";
 import ModuleRealtime from "./ModuleRealtime";
 import { Client } from "@graphand/client";
 
-const accessToken =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoidG9rZW4iLCJpZCI6IjY2ODY5ZDBiNmM4NzM4OGQ4NWVlNzA3ZCIsImlhdCI6MTcyMDA5ODA4NSwiZXhwIjoxNzIwMTAxNjg1fQ.tfPzdctofBPezFjrFrWREMqjUvE0IQaIpvqt_UGuSBI";
-
 describe("ModuleRealtime", () => {
   it("should ...", async () => {
-    expect(true).toBe(true);
-
     const client = new Client([[ModuleRealtime]], {
-      project: "667ed344d28ed7d740c82413",
-      accessToken,
+      project: String(process.env.PROJECT),
+      accessToken: String(process.env.ACCESS_TOKEN),
       headers: {
-        "X-Access-Key": "test123",
+        "X-Access-Key": String(process.env.ACCESS_KEY),
       },
     });
 
@@ -23,16 +18,12 @@ describe("ModuleRealtime", () => {
 
     const socket = client.get("realtime").getSocket();
 
-    console.log(socket);
-
     socket.emit("subscribeModels", "datamodels");
 
     const dm = await client.getModel(DataModel).create({
       slug: "todo2",
       definition: {},
     });
-
-    console.log(dm?.toJSON());
 
     await dm.delete();
 
