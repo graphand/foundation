5b64b53d46d1664f980022f993f224701f3eb54b
pierrecabriere <hello@pierrecabriere.fr>
2024-08-02 21:54:53 +0200

    add basic cli package

---
+++ packages/cli/vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
+import fs from "fs";
+
+export default defineConfig({
+  plugins: [
+    tsconfigPaths(),
+    {
+      name: "chmod-plugin",
+      apply: "build",
+      writeBundle() {
+        fs.chmodSync("/opt/homebrew/bin/graphand", "755");
+      },
+    },
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "@graphand/cli",
+      formats: ["cjs"],
+      fileName: () => `index.js`,
+    },
+    rollupOptions: {
+      external: ["commander", "chalk", "inquirer", "ora"],
+      output: {
+        globals: {},
+      },
+    },
+  },
+});
