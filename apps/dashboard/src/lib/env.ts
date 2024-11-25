37f7126c7c3992e4f6a7a19ee9c08905bc88861b
pierrecabriere <hello@pierrecabriere.fr>
2024-11-25 21:50:36 +0100

    clean

--- apps/dashboard/src/lib/env.ts
+++
@@ -1,7 +0,0 @@
-const env = {
-  APP_HOSTNAME: String(process.env.APP_HOSTNAME),
-  GRAPHAND_CLIENT_ENDPOINT: String(process.env.GRAPHAND_CLIENT_ENDPOINT),
-  GRAPHAND_CLIENT_SSL: String(process.env.GRAPHAND_CLIENT_SSL),
-};
-
-export default env;
--- apps/dashboard/src/lib/headers.ts
+++
@@ -1,9 +0,0 @@
-import { headers } from "next/headers";
-
-export async function getProjectInfo() {
-  const headersList = await headers();
-  return {
-    projectSubdomain: headersList.get("x-project-subdomain") || "",
-    environment: headersList.get("x-environment") || "master",
-  };
-}
